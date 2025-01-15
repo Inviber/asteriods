ba874f5fea1f501d054ab8fd9f2210b4a6c70639
Inviber <bkelley4600@gmail.com>
2025-01-14 19:19:50 -0600

    player is now drawn on the screen

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
         screen.fill(color=(0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
